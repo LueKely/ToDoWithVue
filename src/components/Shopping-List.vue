46ac3119ca4564b14feb7037e640be87e04dbff9
Anunciacion Lue <luekely43110@gmail.com>
2022-09-16 20:49:31 +0800

    added is important

--- src/components/Shopping-List.vue
+++ src/components/Shopping-List.vue
@@ -1,30 +1,44 @@
 <template>
 	<div>
 		<h1>Shopping for my dick wanker</h1>
-		<p v-for="item in items" :key="item.id">-{{ item.name }}</p>
-		<input type="text" placeholder="type-something" v-model.lazy="laman" />
+		<ul>
+			<li v-for="item in items" :key="item.id">{{ item.name }}</li>
+		</ul>
+
+		<input
+			type="text"
+			placeholder="type-something"
+			v-model.lazy="laman"
+			v-on:keyup.enter="timeToPush"
+		/>
+		<input type="checkbox" v-model="isImportant" />
+		<p>is it important? {{ isImportant ? 'yes' : 'no' }}</p>
 		<button @click="timeToPush">send</button>
-		<div>
-			<label><input type="checkbox" value="a" v-model="alphabet" />A</label
-			><label><input type="checkbox" value="b" v-model="alphabet" />B</label
-			><label><input type="checkbox" value="c" v-model="alphabet" />C</label>
-		</div>
-		<div>look it is {{ alphabet }}</div>
 	</div>
 </template>
 
 <script setup>
 	import { ref } from 'vue';
+	const isImportant = ref(false);
 	const laman = ref('');
 	const items = ref([
 		{ id: 1, name: 'ham', isClicked: true },
 		{ id: 2, name: 'ham', isClicked: true },
 		{ id: 3, name: 'ham', isClicked: true },
 	]);
-	const alphabet = ref([]);
-	function timeToPush() {
-		items.value.push({ id: items.value.length, name: laman, isClicked: true });
-	}
+
+	const timeToPush = () => {
+		if (laman.value == '') {
+			alert('please insert something');
+		} else {
+			items.value.push({
+				id: items.value.length,
+				name: laman.value,
+				isClicked: true,
+			});
+			laman.value = '';
+		}
+	};
 </script>
 
 <style scoped>
